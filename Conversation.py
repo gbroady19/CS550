b82e7a50ac6e7cc93ae6f5d50508cfc3977fa50b
Knute Broady <gbroady19@choate.edu>
2018-09-18 15:23:04 -0400

    Added conversation program

---
+++ Conversation.py
@@ -0,0 +1,8 @@
+#Conversation 
+print("Hi, welcome to programming!")
+name = input("What's your name? \n >>")
+print("Nice to meet you, "+name+" my name is Mac-y ")
+response1 = input("What is your favorite subject in school? \n>>")
+print(""+response1+" is really cool! My favorite subject is Computer Science")
+response2=input("Where's your next class? \n>>")
+print("Oh darn! Mine is on the other side of campus from "+response2+". I'll see you later!")
