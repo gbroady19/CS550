e656b7c3c00659e60cfe6800d39677300141783c
Knute Broady <gbroady19@choate.edu>
2018-10-12 16:56:14 -0400

    update to ms.py

--- ms.py
+++ ms.py
@@ -1,27 +1,62 @@
-#minesweeper 
+#minesweeper for CS550 by KB. Completed 10/12/18 
+# Sources consulted: https://www.tutorialspoint.com/python/python_2darray.htm
+#https://stackoverflow.com/questions/7378091/taking-multiple-inputs-from-user-in-python
+#Some help and suggestions provided by Anan Aramthanapon and Kevin Xie 
+# please start by inputting ms.py height width bombs
+
+
 
 import sys 
 import random
  
-height = int(sys.argv[1])+2
+height = int(sys.argv[1])+2 #height and width input from user 
 width = int(sys.argv [2])+2
 bombs = int(sys.argv [3])
 
-solution = [[0] * (width) for x in range(height)]
+solution = [[0] * (width) for x in range(height)] #board creation 
 userboard = [["□"] * (width) for x in range(height)]
-#from https://www.compart.com/en/unicode/U+25A1
-checkcoordinates = [0][0]
-flagcount = 0 
+
+checkcoordinates = [0][0] #Creation of list 
+flagcount = 0
 checklist =[]
 
-def printboard(): 
+global emptyspaces 
+emptyspaces = ((height-2)*(width-2)) - bombs -9   #the number of spaces that don't contain bombs
+
+global revealed
+revealed = 0 #will be increased each time a space is revealed by the user
+
+print(emptyspaces)
+
+def wincheck(): #Checks if you've won after ever revealing sequence 
+	global emptyspaces 
+	global revealed
+	print(revealed)
+
+	if emptyspaces <= revealed:
+
+		print("YOU WIN! Thanks for playing minesweeper!!")
+		winboard()
+		
+	else: 
+		printboard()
+
+def winboard(): #Prints a board so you can see that you've won 
+	solution = userboard
 	for x in range(1,len(userboard)-1):
+		for y in range(1,len(userboard[0])-1):
+			print(userboard[x][y],end=" ")
+		print("")
+	quit()
+
+def printboard(): 
+	for x in range(1,len(userboard)-1): #prints board for other purposes
 		for y in range(1,len(userboard[0])-1):
 			print(userboard[x][y],end=" ")
 		print("")
 	selecttile()
 
-def selecttile(): 
+def selecttile(): #allows user to select tile for flagging or clearing 
 	global coordinates
 	global flagcount
 	userinput = input("Press F to place a flag, Press C to clear\n\n>>")
@@ -33,17 +68,22 @@ def selecttile():
 		flagcount += 1 
 		printboard() 
 
-	if userinput == "C":
-		tileselection= input("Enter two corodinates separated by commas: ")
+	if userinput == "C": 
+		global revealed
+		tileselection= input("Please Enter the coordinate  Y,X format ")
 		tile_input= tileselection.split(',')
 		coordinates = [int(x.strip()) for x in tile_input]
 		userboard[coordinates[0]][coordinates[1]] = solution[coordinates[0]][coordinates[1]] 
+		revealed +=3	
 		bombchecking()
 
-def bombchecking(): 
+	else: 
+		print("I don't know what that means")
+		selecttile()
+
+def bombchecking(): #checks if user input was a bomb
 	if userboard[coordinates[0]][coordinates[1]] == "*":
-		print("""That was a BOMB, you lose""")
-#https://asciiart.website/index.php?art=objects/explosives
+		print("That was a BOMB, you lose")
 		quit()
 
 	elif userboard[coordinates[0]][coordinates[1]] == 0:
@@ -53,15 +93,18 @@ def bombchecking():
 		zeroreveal()
 
 	else: 
-		printboard()
-
-def zeroreveal():
+		wincheck()
 	
+
+def zeroreveal(): #if userinput was a zero, reveal all spaces around it, do the same for all zero it finds 
+	global revealed
 	while len(checklist) > 0:
+		
 		coordinate1 = checklist[0]
 		coordinate2 = checklist[1]
 		checklist.pop(0)
 		checklist.pop(0)
+		revealed +=1
 
 		for x in range(-1, 2):
 			for y in range(-1, 2):
@@ -70,119 +113,24 @@ def zeroreveal():
 						checklist.append(coordinate1+x)
 						checklist.append(coordinate2+y)
 						userboard[coordinate1+(x)][coordinate2+(y)]=  solution[coordinate1+(x)][coordinate2+(y)]
+					
+	
 					else: 
 						pass 
 				else: 
 					userboard[coordinate1+(x)][coordinate2+(y)]=  solution[coordinate1+(x)][coordinate2+(y)] 
+					
+
 		zeroreveal()
-		
-		printboard()
+		wincheck()
 
-				
-
-
-
-
-
-
-
-
-# def continuouscheck(x,y):
-# 	print(x,y)
-# 	if 0<x<width-1 and 0<y<height-1: #onboard
-# 		if solution[x+1][y] == 0: # is a zero
-# 			if solution[x+1][y] == userboard[x+1][y]: # is already revealed
-# 				print("nope") 
-# 			else: #not revealed
-# 				userboard[x+1][y] = solution[x+1][y] # reveal
-# 				print("leave recursion 1") 	
-# 				continuouscheck(x+1,y) # check around
-# 				print("return recursion 1")
-# 		else: 
-# 			solution[x+1][y]=userboard[x+1][y]
-
-# 		if solution[x][y+1] == 0:
-# 			if solution[x][y+1] == userboard[x][y+1]:
-# 				print("nope")
-# 			else:  
-# 				userboard[x][y+1] = solution[x][y+1] 
-# 				print("leave recursion 2") 	
-# 				continuouscheck(x,y+1)
-# 		else: 
-# 			userboard[x][y+1] = solution[x][y+1] 
-		
-# 		if solution[x+1][y+1] == 0:
-# 			if userboard[x+1][y+1] == solution[x+1][y+1]:
-# 				print("nope") 
-# 			else:
-# 				userboard[x+1][y+1] = solution[x+1][y+1]
-# 				print("leave recursion 3") 	
-# 				continuouscheck(x+1,y+1)
-# 		else: 
-# 			userboard[x+1][y+1] = solution[x+1][y+1] 
-
-# 		if solution[x-1][y] == 0:
-# 			if userboard[x-1][y] == solution[x-1][y]:
-# 				print("nope")
-# 			else: 
-# 				userboard[x-1][y] = solution[x-1][y] 
-# 				print("leave recursion 4") 	
-# 				continuouscheck(x-1,y)	
-
-# 		else: 
-# 			userboard[x-1][y] = solution[x-1][y] 
 			
-		
-# 		if userboard[x-1][y-1] == 0: 
-# 			if solution[x-1][y-1]== userboard[x-1][y-1]:
-# 				print("nope")
-# 			else:
-# 				solution[x-1][y-1]= userboard[x-1][y-1]
-# 				print("leave recursion 5") 	
-# 				continuouscheck(x-1,y-1)
-		
-# 		else: 
-# 			solution[x-1][y-1]= userboard[x-1][y-1]
 
-# 		if userboard[x][y-1] == 0:
-# 			if solution[x][y-1]== userboard[x][y-1]: 
-# 				print("nope") 
-# 			else: 
-# 				solution[x][y-1]= userboard[x][y-1]
-# 				print("leave recursion 6") 	
-# 				continuouscheck(x,y-1)
-# 		else: 
-# 			solution[x][y-1]= userboard[x][y-1]
-
-
-# 		if userboard[x+1][y-1] == 0:
-# 			if solution[x+1][y-1]== userboard[x][y-1]: 
-# 				print("nope") 
-# 			else: 
-# 				solution[x+1][y-1]= userboard[x][y-1]
-# 				print("leave recursion 7") 	
-# 				continuouscheck(x+1,y-1)
-# 		else: 
-# 			solution[x+1][y-1]= userboard[x+1][y-1]
-
-
-# 		if userboard[x-1][y+1] == 0:
-# 			if solution[x-1][y+1]== userboard[x][y-1]: 
-# 				print("nope") 
-# 			else: 
-# 				solution[x-1][y+1]= userboard[x][y-1]
-# 				print("leave recursion 8") 	
-# 				continuouscheck(x-1,y+1)
-# 		else: 
-# 			solution[x-1][y+1]= userboard[x-1][y+1]
-
-# 	printboard()
-
-
-for x in range(bombs):
+for x in range(bombs): #bomb placement
 	solution[int(random.randrange(1, height
 -1))] [int(random.randrange(1,width-1))] = '*'
 
+#boarder of ones to avoid index errors
 solution[0] = [1]*(width) 
 solution[-1] = [1]*(width)
 
@@ -191,8 +139,8 @@ for j in solution:
 	j[-1]= 1
 
 
-
-for x in range(1,height-1):
+#creation of initial solution boards
+for x in range(1,height-1): 
 	for y in range(1,width-1):  
 
 		if solution[x][y] == "*": 
@@ -216,7 +164,6 @@ for x in range(1,height-1):
 
 printboard()
 
-#Much help from Anan Aramthanapon 
 
 
 
@@ -225,8 +172,6 @@ printboard()
 
 
 
-# https://www.tutorialspoint.com/python/python_2darray.htm
-#https://stackoverflow.com/questions/7378091/taking-multiple-inputs-from-user-in-python
 
 
 
