ac5360001abb6e15daf7adaf37f360530432dca2
Knute Broady <gbroady19@choate.edu>
2018-10-15 14:10:50 -0400

    updated commenting

--- Colors.py
+++ Colors.py
@@ -23,14 +23,17 @@ outerColor3=(200)
 
 for x in range(imgx):
 	for y in range(imgy):
-
+						#Distance to endge, subtracted from, Greatest value of distance to edge calculation 
 		distanceToEdge = max(abs(x - imgx), x, abs(y - imgy), y)- min(abs(x - imgx), x, abs(y - imgy), y)
+		
+		#Make it on a scale from 0 to 1 to allow for a smootth transition 
 		distanceToEdge = float(distanceToEdge) / (imgx/2)
 
-        #Make it on a scale from 0 to 1
+       
 
 
-        #Calculate r, g, and b values
+        #Calculate r, g, and b values that allow the inner color to slowly become outer color. as the distance to edge becomes closer to zero
+        #the innercolor value gets closer to zero, so the number approaches the outer color. 
 		r = innerColor * distanceToEdge + outerColor * (1 - distanceToEdge)
 		g = innerColor2 * distanceToEdge + outerColor2 * (1- distanceToEdge)
 		b = innerColor3 * distanceToEdge + outerColor3 * (1 - distanceToEdge)
