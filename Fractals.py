0a67e3ac3f3fc02127bbcd3e6ab07a81074afb72
Knute Broady <gbroady19@choate.edu>
2018-10-24 21:48:15 -0400

    Fractals

--- Fractals.py
+++ Fractals.py
@@ -1,5 +1,5 @@
 '''
-CS550 Project by KB
+CS550 Fractal Project by KB
 10/25/18 
 This project includes two interesting mandelbrot fractels and one fractal based loosely on the Cantor set 
 It also includes experimentation with colors and other functions availible in the PIL library 
@@ -14,7 +14,7 @@ from PIL import ImageColor as I
 from PIL import ImageOps
 from PIL import ImageFilter
 
-#Recursively drawn line based fractal loosely based on Cantor set 
+#Recursively drawn line based fractal loosely based on Cantor set. Mainly inspired by image on  Nature of Code website linked below
 #Sources: 
 #https://natureofcode.com/book/chapter-8-fractals/
 #https://stackoverflow.com/questions/13053443/drawing-a-line-on-an-image-with-pil/13053545
@@ -36,14 +36,14 @@ def drawOut(x, y, dx, dy, num, den):
 	#if the change in distance becomes 0, stop drawing
 	if dx == 0 and dy == 0:
 		return
-	#draw an inital starting line in two directions  
+	#draw an inital starting line in two directions, random colors using HSV
 	draw.line((x, y, x+dx, y+dy), fill=(r.randrange(0,361),100, 200), width = 1)
 	draw.line((x, y, x-dx, y-dy), fill=(r.randrange(0,361),100, 200), width = 1)
 	#draw a line from the end point of intial line, except at x and y are switched (drawn at a 90 degree angle) in both directions and length is decreased by a certain fraction (num/den). Repeats recusively until length is decreased to zero 
 	drawOut(x+dx, y+dy, (num*dy)//den, (num*dx)//den, num, den)
 	drawOut(x-dx, y-dy, (num*dy)//den, (num*dx)//den, num, den)
 
-#Initial calling of function, starting lines are drawn at the center of the image. The initial num and dem were chosen after experiementation to produce the most interetesing looking fractal
+#Initial calling of function, starting lines are drawn at the center of the image. The initial num and dem were chosen after experiementation to produce the most interetesing looking fractal. 23/32 produces the maximum number of lines without overlap. 
 drawOut(imgx//2, imgy//2, imgx//6, 0, 23, 32)
 
 #crops image to make the fractal easier to see
@@ -74,7 +74,7 @@ for Py in range(imgy):
 		y = 0 
 		iteration = 0 
 		#set maximum number of iterations
-		max_iteration = 256
+		max_iteration = 400
 		while (x*x + y*y <= 2) and iteration < max_iteration: 
 			#calculations based on wikihow
 			xtemp = x*x - y*y + xS
@@ -87,12 +87,15 @@ for Py in range(imgy):
 		colorB = 256- iteration
 		image.putpixel((Px,Py),(colorR, colorG, colorB))
 		
-#Interesting function in ImageOps in PIL that inverts pixels above a certain threshold value. 
+#Interesting function in ImageOps in PIL that inverts pixels above a certain threshold value, shows image. 
 ImageOps.solarize(image, threshold=240).show()
 
 #Mandelbrot 3
 xa, xb = -0.6761486325412989, -0.6649729572236538
 ya, yb = -0.46341093443341, -0.45223525911569595
+
+imgx = 500
+imgy = 500
 image = Image.new("HSV",(imgx,imgy))
 
 #for all the pixels in the image
@@ -105,19 +108,19 @@ for Py in range(imgy):
 		y = 0 
 		iteration = 0 
 		#set maximum number of iterations
-		max_iteration = 256
+		max_iteration = 400
 		while (x*x + y*y <= 2) and iteration < max_iteration: 
 			#calculations based on wikihow
 			xtemp = x*x - y*y + xS
 			y = 2*x*y + yS 
 			iteration += 1 
 			x = xtemp
-		# Played around with colorspace. Although calculation is simple, using the right combination of ImgX variable, Px variable and the all important iteration variable 
+		# Played around with colorspace. Although calculation is simple, finding the right combination of ImgX variable, Px variable and the important iteration variable to acheive the desired affect did take much experimentation 
 		Color= (iteration%360)%int(360/imgx*Px+1)
 		Saturation = 360%int(iteration*2)
 		Brightness= 300
 		colorB = 256- iteration
 		image.putpixel((Px,Py),(Color, Saturation, Brightness))
-
+#shows image 
 image.show()
 
